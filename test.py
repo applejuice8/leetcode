013fda920a0d5968e31b68bbeefae2a0e835d8a2
Colin <colinleonghongseng@gmail.com>
2025-10-31 19:18:02 +0800

    Removed test.py

--- test.py
+++
@@ -1,9 +0,0 @@
-lows = [1,2,3,4,5]
-highs = [9,7,5,7,8]
-
-for low in lows:
-    for high in highs:
-        mid = low + (high - low) // 2
-        print(f'{mid}, ', end='')
-        mid = (low + high) // 2
-        print(mid)
